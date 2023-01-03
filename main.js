734cbe336f6a5cc2549bd55a3f94dab680e4c1f1
pavanhumai <121843326+pavanhumai@users.noreply.github.com>
2023-01-03 09:43:14 +0530

    kesjbvrzlgxreuibkv

---
+++ main.js
@@ -0,0 +1,9 @@
+function edit(){
+    document.getElementById("form1").style.display = "none";
+    document.getElementById("form2").style.display = "block";
+
+}
+function save(){
+    document.getElementById("form2").style.display = "none";
+    document.getElementById("form1").style.display = "block";
+}
